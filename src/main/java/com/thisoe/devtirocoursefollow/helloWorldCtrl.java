1452f44de9f8b23629de077f20c21a22289f2531
Thisoe <thisoecode@gmail.com>
2024-02-11 19:14:20 +0900

    Hello world controller done

---
+++ src/main/java/com/thisoe/devtirocoursefollow/helloWorldCtrl.java
@@ -0,0 +1,12 @@
+package com.thisoe.devtirocoursefollow;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class helloWorldCtrl {
+    @GetMapping(path="/hello")
+    public static String helloWorld() {
+        return "Hello world!";
+    }
+}
